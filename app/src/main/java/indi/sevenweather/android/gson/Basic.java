e6470ed74a5ffd4b762c20a76ddb10bf402cb77c
SuPersonic-Levi <1091621490@qq.com>
2022-04-15 14:44:40 +0800

    定义GSON所有实体类

---
+++ app/src/main/java/indi/sevenweather/android/gson/Basic.java
@@ -0,0 +1,15 @@
+package indi.sevenweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city") //JSON中的有些字段不太适合作为java字段就用这个注解的方式来在json和java字段间建立映射关系
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
